a12ab45515aee4f948ea7826e69f23831367d445
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-02 09:40:11 -0700

    added  more rob ust test for doctors show spec

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -9,18 +9,27 @@ RSpec.describe "Doctors show page", type: :feature do
     doctor_3 = hospital_2.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford")
     patient_1 = doctor_1.patients.create(name: "Katie Bryce", age: "24")
     patient_2 = doctor_1.patients.create(name: "Denny Duquette", age: "39")
+    patient_3 = doctor_2.patients.create(name: "Danny DeVito", age: "56")
 
 
-    visit '/doctors/'
+    visit "/doctors/#{doctor_1.id}"
 
-
+    expect(page).to have_content(doctor_1.name)
+    expect(page).to have_content(doctor_1.specialty)
+    expect(page).to have_content(doctor_1.university)
+    expect(page).to have_content(hospital_1.name)
+    expect(page).to have_content(doctor_1.name)
+    expect(page).to have_content(patient_1.name)
+    expect(page).to have_content(patient_2.name)
+    expect(page).to_not have_content(patient_3.name)
+    expect(page).to_not have_content(doctor_2.name)
+    expect(page).to_not have_content(doctor_2.specialty)
+    expect(page).to_not have_content(doctor_2.university)
+    expect(page).to_not have_content(hospital_2.name)
   end
 end
 
-# User Story 1, Doctors Show Page
-# ​
-# As a visitor
-# When I visit a doctor's show page
+
 # I see all of that doctor's information including:
 #   - name
 #   - specialty
