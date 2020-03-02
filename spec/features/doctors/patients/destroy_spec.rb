8a4f0e3b83c0b110c420dc142fb4a6d1424e0cd7
David Atkins <atki1080@gmail.com>
2020-03-02 10:08:09 -0700

    Add doctor/patients destroy spec

---
+++ spec/features/doctors/patients/destroy_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'from the doctor show page' do
+    it "I see remove a patient from a doctor's caseload" do
+      hospital1 = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+      hospital2 = Hospital.create!(name: 'Pacific Northwest General Hospital')
+      doctor1 = Doctor.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University', hospital: hospital1)
+      doctor2 = Doctor.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University', hospital: hospital1)
+      doctor3 = Doctor.create!(name: 'Miranda Bailey', specialty: 'General Surgery', university: 'Stanford University', hospital: hospital2)
+      patient1 = Patient.create!(name: 'Katie Bryce', age: 24)
+      patient2 = Patient.create!(name: 'Denny Duquette', age: 39)
+      patient3 = Patient.create!(name: 'Rebecca Pope', age: 32)
+      patient4 = Patient.create!(name: 'Zola Shepherd', age: 2)
+      hospital1.doctors << [doctor1, doctor2]
+      hospital2.doctors << [doctor3]
+      doctor1.patients << [patient1, patient3, patient4]
+      doctor2.patients << [patient2]
+
+      visit doctor_path(doctor1.id)
+
+      within("#patient-#{patient4.id}") { click_button ('Remove this patient') }
+
+      expect(current_path).to eq(doctor_path(doctor1.id))
+
+      expect(page).to_not have_css("#patient-#{patient4.id}")
+      expect(page).to have_css("#patient-#{patient1.id}")
+      expect(page).to have_css("#patient-#{patient3.id}")
+    end
+  end
+end
