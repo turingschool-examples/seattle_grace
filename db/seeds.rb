acae786210843a3938acda243eca792d33f65807
William Meighan <williamdmeighan@gmail.com>
2020-03-02 09:41:35 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
+
+doctor = hospital.doctors.create(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+
+patient_1 = doctor.patients.create(name: "Katie Bryce", age: 24)
+patient_2 = doctor.patients.create(name: "Denny Duquette", age: 39)
