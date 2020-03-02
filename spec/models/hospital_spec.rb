56433d75449d6bbb3c9df7fcd20bcb4c371cf71b
Nathan Keller <keller.nathan@gmail.com>
2020-03-02 10:42:14 -0700

    User story 3 complete.

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -48,8 +48,52 @@ RSpec.describe Hospital, type: :model do
           university: "Johns Hopkins University",
           hospital: hospital_1
         )
-        
+
       expect(hospital_1.count_doctors).to eq(3)
+    end
+
+    it "can provide a list of the unique universities of the doctors" do
+
+      Hospital.destroy_all
+      Doctor.destroy_all
+
+      hospital_1 = Hospital.create(
+        name: "Grey Sloan Memorial Hospital"
+        )
+
+      hospital_2 = Hospital.create(
+        name: "Pacific Northwest General Hospital"
+        )
+
+      doctor_1 = Doctor.create!(
+          name: "Meredith Grey",
+          specialty: "General Surgery",
+          university: "Harvard",
+          hospital: hospital_1
+        )
+
+      doctor_2 = Doctor.create!(
+          name: "Alex Karev",
+          specialty: "Pediatric Surgery",
+          university: "Johns Hopkins University",
+          hospital: hospital_1
+        )
+
+      doctor_3 = Doctor.create!(
+          name: "Miranda Bailey",
+          specialty: "General Surgery",
+          university: "Stanford University",
+          hospital: hospital_2
+        )
+
+      doctor_4 = Doctor.create!(
+          name: "Not a doctor. Just need a university",
+          specialty: "Soemthing",
+          university: "Johns Hopkins University",
+          hospital: hospital_1
+        )
+
+      expect(hospital_1.distinct_universities).to eq(["Harvard", "Johns Hopkins University"])
     end
   end
 end
