62116f1d883767a9d736c7abde09be78be475eb4
DeMarcus Kirby <tkd024@gmail.com>
2020-03-02 10:19:20 -0700

    update model to have num of doctors and universities helper methods

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital <ApplicationRecord
   validates_presence_of :name
   has_many :doctors
+
+  def num_of_doctors
+    doctors.count
+  end
+
+  def universities
+    doctors.pluck(:university)
+  end
 end
