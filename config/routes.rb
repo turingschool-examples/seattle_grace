f23c57aea178cab0a025217cf716e013aeda98ac
David Atkins <atki1080@gmail.com>
2020-03-02 10:22:40 -0700

    Add doctor_patients destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :doctors, only: [:show]
+  resources :doctor_patients, only: [:destroy]
   resources :patients, only: [:index]
   resources :hospitals, only: [:show]
 end
