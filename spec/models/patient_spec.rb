2107736185217899038a1a7228cd29fafb7033f9
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-02 11:27:54 -0700

    Test patient model method to order by age

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe Patient, type: :model do
     it {should have_many :patient_doctors}
     it {should have_many(:doctors).through(:patient_doctors)}
   end
+
+  describe 'methods' do
+    it "ordered_by_age" do
+      p3 = Patient.create!({
+        name: "Jan",
+        age: 30
+        })
+      p4 = Patient.create!({
+        name: "Mike",
+        age: 35
+        })
+      p5 = Patient.create!({
+        name: "Becca",
+        age: 52
+        })
+
+      expect(Patient.ordered_by_age).to eq([p5,p4,p3])
+    end
+  end
 end
