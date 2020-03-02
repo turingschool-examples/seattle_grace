1d3484671e5f3f596e68c772369d4a5efd5342f3
Nathan Keller <keller.nathan@gmail.com>
2020-03-02 09:22:24 -0700

    Many to many established

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20200302160452) do
   enable_extension "plpgsql"
 
   create_table "doctor_patients", force: :cascade do |t|
-    t.bigint "doctors_id"
-    t.bigint "patients_id"
-    t.index ["doctors_id"], name: "index_doctor_patients_on_doctors_id"
-    t.index ["patients_id"], name: "index_doctor_patients_on_patients_id"
+    t.bigint "doctor_id"
+    t.bigint "patient_id"
+    t.index ["doctor_id"], name: "index_doctor_patients_on_doctor_id"
+    t.index ["patient_id"], name: "index_doctor_patients_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20200302160452) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "doctor_patients", "doctors", column: "doctors_id"
-  add_foreign_key "doctor_patients", "patients", column: "patients_id"
+  add_foreign_key "doctor_patients", "doctors"
+  add_foreign_key "doctor_patients", "patients"
   add_foreign_key "doctors", "hospitals"
 end
