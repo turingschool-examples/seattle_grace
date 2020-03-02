2add1c680c8332e4070fde27cc91a9adf66ba653
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-02 09:11:32 -0700

    Finalize relationships and validations for patients

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,7 @@
 class Doctor < ApplicationRecord
   validates_presence_of :name, :specialty, :university
+  
+  has_many :patients
+
   belongs_to :hospital
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,5 @@
-class Patient
-  
+class Patient < ApplicationRecord
+  validates_presence_of :name, :age
+
+  belongs_to :doctor
 end
