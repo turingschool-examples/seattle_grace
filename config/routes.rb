cb3bb2ade5d52a6b2712be9284ed36f3cbc4786b
DeMarcus Kirby <tkd024@gmail.com>
2020-03-02 11:27:10 -0700

    Update route to doctor_patients controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/doctors/:id', to: 'doctors#show'
+  delete '/doctors/:doctor_id/patients/:id', to: 'doctor_patients#destroy'
 
   get '/patients', to: 'patients#index'
-  delete '/patients/:id', to: 'patients#destroy'
 
   get '/hospitals/:id', to: 'hospitals#show'
 end
