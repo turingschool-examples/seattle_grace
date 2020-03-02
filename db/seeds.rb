fc343201efb24742fe4b486b8e8928ecd212056a
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-02 11:14:36 -0700

    add seeds for rails s testing and clean up views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hospital.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+
+hospital1 = Hospital.create!(name: 'Hospital in Denver')
+hospital2 = Hospital.create!(name: 'Hospital in Boulder')
+doctor1 = hospital1.doctors.create!(name: 'John', specialty: 'Surgeon', university: 'Harvard')
+doctor2 = hospital2.doctors.create!(name: 'Steve', specialty: 'Adults', university: 'Yale')
+doctor3 = hospital2.doctors.create!(name: 'Chris', specialty: 'Children', university: 'Yale')
+doctor4 = hospital2.doctors.create!(name: 'Larry', specialty: 'Life', university: 'Stanford')
+patient1 = doctor1.patients.create!(name: 'Christian', age: 28)
+patient2 = doctor2.patients.create!(name: 'Tanner', age: 26)
+patient3 = doctor2.patients.create!(name: 'Megan', age: 22)
+patient2 = doctor2.patients.create!(name: 'Sally', age: 42)
