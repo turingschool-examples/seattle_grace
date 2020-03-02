72a22c1f4387d2d49d2dabebd1869a6a2140f976
Matt Holmes <holmesm8@gmail.com>
2020-03-02 09:53:23 -0700

    Add model testing for patient ordering

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Patient, type: :model do
     it {should have_many :doctor_patients}
     it {should have_many(:doctors).through(:doctor_patients)}
   end
+
+  describe "methods" do
+    it "can order patients by age" do
+      pt1 = Patient.create!(name: "Kathleen", age: 10)
+      pt2 = Patient.create!(name: "Zeke", age: 20)
+      pt3 = Patient.create!(name: "Meg", age: 21)
+
+      expected_result = [pt3, pt2, pt1]
+      expect(Patient.oldest_to_youngest).to eq(expected_result)
+    end
+  end
 end
