34feca91b6f510d857ab774e4b10fd6afcfed842
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-02 11:27:55 -0700

    Add model test for doctor count by hospital

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -33,4 +33,30 @@ RSpec.describe Hospital, type: :model do
 
     expect(seattle_grace.educations).to eq([meredith.university, karev.university])
   end
+
+  it 'doctor_count' do
+    seattle_grace = Hospital.create(name: 'Seattle Grace Hospital')
+    katie = Patient.create(name: 'Katie Bryce', age: 24)
+    denny = Patient.create(name: 'Denny Duquette', age: 39)
+    rebecca = Patient.create(name: 'Rebecca Pope', age: 32)
+    meredith = Doctor.create(name: 'Meredith Grey',
+                             specialty: 'General Surgery',
+                             university: 'Harvard University',
+                             hospital: seattle_grace,
+                             patients: [denny, katie])
+
+    karev = Doctor.create(name: 'Alex Karev',
+                             specialty: 'Pediatric Surgery',
+                             university: 'Johns Hopkins University',
+                             hospital: seattle_grace,
+                             patients: [denny, katie])
+
+    karev2 = Doctor.create(name: 'Alex Karev 2',
+                            specialty: 'Pediatric Surgery',
+                            university: 'Johns Hopkins University',
+                            hospital: seattle_grace,
+                            patients: [denny, katie])
+
+    expect(seattle_grace.doctor_count).to eq(3)
+  end
 end
