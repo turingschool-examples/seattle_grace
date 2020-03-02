86b9b26e0e5d6024da5f7b4db66c5315446dbeea
Matt Holmes <holmesm8@gmail.com>
2020-03-02 09:59:47 -0700

    Finish user story 2

--- spec/feature/patient_index_spec.rb
+++ spec/feature/patient_index_spec.rb
@@ -3,13 +3,10 @@ require 'rails_helper'
 RSpec.describe 'as a visitor', type: :feature do
   describe 'when I visit the patient index page' do
     it 'can see all of the patients listed from oldest to youngest' do
-      hs1 = Hospital.create!(name: "Johns Hopkins")
-      hs2 = Hospital.create!(name: "Hope")
-      dr1 = hs1.doctors.create!(name: "Matt", specialty: "brains", university: "GVSU", )
-      dr2 = hs2.doctors.create!(name: "Meghan", specialty: "holistics", university: "Community College")
-      pt1 = dr1.patients.create!(name: "Kathleen", age: 10)
-      pt2 = dr1.patients.create!(name: "Zeke", age: 20)
-      pt3 = dr2.patients.create!(name: "Meg", age: 21)
+
+      pt1 = Patient.create!(name: "Kathleen", age: 10)
+      pt2 = Patient.create!(name: "Zeke", age: 20)
+      pt3 = Patient.create!(name: "Meg", age: 21)
 
       visit "/patients"
       expect(page).to have_content(pt1.name)
