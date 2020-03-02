90dcebaa96b6534dc290bce4529bdda0a78baff7
Ben Fox <benfox1216@gmail.com>
2020-03-02 10:35:47 -0700

    Add model test for new patient model method

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,8 +5,27 @@ describe Patient, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
   end
+  
   describe 'relationships' do
     it {should have_many :doctor_patients}
     it {should have_many(:doctors).through(:doctor_patients)}
+  end
+  
+  describe 'models' do
+    before :each do
+      @grey = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      @meredith = @grey.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+      
+      @katie = @meredith.patients.create!(name:"Katie Bryce", age: 24)
+      @denny = @meredith.patients.create!(name:"Denny Duquette", age: 39)
+      @rebecca = @meredith.patients.create!(name:"Rebecca Pope", age: 32)
+      @zola = @meredith.patients.create!(name:"Zola Shepherd", age: 2)
+    end
+    
+    it ".in_order" do
+      expect(Patient.in_order.count).to eq(4)
+      expect(Patient.in_order.first).to eq(@denny)
+      expect(Patient.in_order.last).to eq(@zola)
+    end
   end
 end
