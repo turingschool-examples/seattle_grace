a3b84dd5cdf94f87132b17923d20f09d1e58e7b1
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-02 10:41:16 -0700

    Add user story 3

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital <ApplicationRecord
   validates_presence_of :name
   has_many :doctors
+
+  def number_of_doctors
+    doctors.count
+  end
+
+  def universities_attended
+    doctors.select(:university).distinct.pluck(:university)
+  end
 end
