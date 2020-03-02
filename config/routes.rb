1adceb2701b81263bb13906392f56a3547f52aba
William Meighan <williamdmeighan@gmail.com>
2020-03-02 10:51:15 -0700

    Add destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :doctors, only: [:show]
-  resources :patients, only: [:index]
+  resources :patients, only: [:index, :destroy]
   resources :hospitals, only: [:show]
 end
