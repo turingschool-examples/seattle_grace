7a323b65ac558872d8b7ddc85233c56884b1e850
DeMarcus Kirby <tkd024@gmail.com>
2020-03-02 10:18:45 -0700

    add test for num of docctors and universities model methods

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Hospital, type: :model do
   describe 'relationships' do
     it {should have_many :doctors}
   end
+
+  describe 'methods' do
+    before :each do
+      @hospital = Hospital.create!(name: 'Parkland Hospital')
+      @tron = @hospital.doctors.create!(name: 'Dr. Tron Kattington', specialty: 'Attending Surgeon', university: 'University of Mind Ya Business')
+      @kat = @hospital.doctors.create!(name: 'Dr. Katton Tronson', specialty: 'Attending Surgeon', university: 'University of Why You Askin')
+      @patient1 = @tron.patients.create!(name: 'Joe', age: 20)
+      @patient2 = @tron.patients.create!(name: 'Becky', age: 22)
+      @patient3 = @kat.patients.create!(name: 'John', age: 20)
+    end
+
+    it '#num_of_doctors' do
+      expect(@hospital.num_of_doctors).to eq(2)
+    end
+
+    it '#universities' do
+      expect(@hospital.universities).to eq(["University of Mind Ya Business", "University of Why You Askin"])
+    end
+  end
 end
