ec0075b195038cbda08d82afeedc10b25c3c3522
Jordan <jordan.wallace.williams@gmail.com>
2020-03-02 11:38:50 -0700

    Remove unnecessary line

--- app/controllers/patient_doctors_controller.rb
+++ app/controllers/patient_doctors_controller.rb
@@ -2,7 +2,6 @@ class PatientDoctorsController < ApplicationController
   def destroy
     @patient = Patient.find(params[:id])
     @doctor = Doctor.find(params[:doctor_id])
-    doctor = @patient.doctors.first
     if @doctor.patients.delete(@patient)
       flash[:success] = 'Patient removed.'
       redirect_to doctor_path(doctor)
