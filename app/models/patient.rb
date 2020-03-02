6163ba98a3a40e3e1782841618315d0bd8568876
David Atkins <atki1080@gmail.com>
2020-03-02 09:43:04 -0700

    Add by_age method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,8 @@ class Patient <ApplicationRecord
   validates_presence_of :name, :age
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
+
+  def self.by_age
+    order(age: :desc)
+  end
 end
