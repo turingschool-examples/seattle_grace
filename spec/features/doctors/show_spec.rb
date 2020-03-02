72b74edf48ba0d5f033ff4d640435cd4c81e02ad
Ben Fox <benfox1216@gmail.com>
2020-03-02 10:00:54 -0700

    Finish user story 1, doctor show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'As a visitor' do
-  describe 'when I visit a doctor show page'
+  describe 'when I visit a doctor show page' do
     before :each do
       @grey = Hospital.create!(name: "Grey Sloan Memorial Hospital")
       @meredith = @grey.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
@@ -12,6 +12,17 @@ describe 'As a visitor' do
     end
     
     it "I see that doctors information" do
-      expect(page).to have_content(@meredith.name)
+      expect(page).to have_content("Doctor: #{@meredith.name}")
+      expect(page).to have_content("Specialty: #{@meredith.specialty}")
+      expect(page).to have_content("Education: #{@meredith.university}")
+      expect(page).to have_content("Hospital: #{@meredith.hospital.name}")
+      
+      within ("#patients") do
+        expect(page).to have_content("Patients:")
+        expect(page).to have_content("#{@katie.name}")
+        expect(page).to have_content("#{@denny.name}")
+      end
+      save_and_open_page
     end
+  end
 end
