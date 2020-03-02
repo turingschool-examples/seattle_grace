672cb02c4c426b32fcecd49b2cc0033bc8d12f93
William Meighan <williamdmeighan@gmail.com>
2020-03-02 10:04:43 -0700

    Create feature test to display total doctors

--- spec/features/hospital/show_spec.rb
+++ spec/features/hospital/show_spec.rb
@@ -4,16 +4,21 @@ describe 'As a visitor on the hospital show page' do
   before :each do
     @hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
 
-    @doctor = @hospital.doctors.create(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+    @doctor_1 = @hospital.doctors.create(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+    @doctor_2 = @hospital.doctors.create(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
 
-    @patient_1 = @doctor.patients.create(name: "Katie Bryce", age: 24)
-    @patient_2 = @doctor.patients.create(name: "Denny Duquette", age: 39)
+    @patient_1 = @doctor_1.patients.create(name: "Katie Bryce", age: 24)
+    @patient_2 = @doctor_1.patients.create(name: "Denny Duquette", age: 39)
 
     visit "/hospitals/#{@hospital.id}"
   end
 
   it "I see the hospital name" do
     expect(page).to have_content("#{@hospital.name}")
+  end
+
+  it "I see the number od doctors at this hospital" do
+    expect(page).to have_content("Number of doctors: 2")
   end
 
 end
