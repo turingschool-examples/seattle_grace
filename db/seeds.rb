061d5a60f158ca960279a10d02685a90bf21e860
David Atkins <atki1080@gmail.com>
2020-03-02 10:27:15 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+DoctorPatient.destroy_all
+Patient.destroy_all
+Doctor.destroy_all
+Hospital.destroy_all
+hospital1 = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+hospital2 = Hospital.create!(name: 'Pacific Northwest General Hospital')
+doctor1 = Doctor.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University', hospital: hospital1)
+doctor2 = Doctor.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University', hospital: hospital1)
+doctor3 = Doctor.create!(name: 'Miranda Bailey', specialty: 'General Surgery', university: 'Stanford University', hospital: hospital2)
+patient1 = Patient.create!(name: 'Katie Bryce', age: 24)
+patient2 = Patient.create!(name: 'Denny Duquette', age: 39)
+patient3 = Patient.create!(name: 'Rebecca Pope', age: 32)
+patient4 = Patient.create!(name: 'Zola Shepherd', age: 2)
+hospital1.doctors << [doctor1, doctor2]
+hospital2.doctors << [doctor3]
+doctor1.patients << [patient1, patient3, patient4]
+doctor2.patients << [patient2]
