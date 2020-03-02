e5286a519d18928cea763d06c6a41238d9ab62a6
Elom Amouzou <elom1990@yahoo.com>
2020-03-02 11:07:51 -0700

    add destroy action and helper method; all tests pass

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,20 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.all
   end
+
+  def destroy
+    patient = Patient.find(params[:id])
+    doctor = Doctor.find(params[:doctor_id]) if params[:doctor_id] != nil
+    remove_all_traces_of_patient(doctor, patient)
+    redirect_to "/doctors/#{doctor.id}"
+  end
+
+
+  private
+
+  def remove_all_traces_of_patient(doctor, patient)
+    doctor_patient = DoctorPatient.where(doctor_id: doctor.id, patient_id: patient.id).first
+    doctor_patient.destroy
+    patient.destroy
+  end
 end
