4c0e78f642607c609c74b0ba4d3b9b42bc5ef755
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-02 11:50:08 -0700

    remove unnecessary variable in destroy method

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -1,7 +1,7 @@
 class DoctorPatientsController < ApplicationController
   def destroy
     doctor = Doctor.find(params[:id])
-    patient = DoctorPatient.find_by(doctor_id: params[:id], patient_id: params[:patient_id]).destroy
+    DoctorPatient.find_by(doctor_id: params[:id], patient_id: params[:patient_id]).destroy
     redirect_to "/doctors/#{doctor.id}"
   end
 end
