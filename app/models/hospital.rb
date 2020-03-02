98e069ae277c52e2ef5827c3d28faa7c7b8f3798
Matt Holmes <holmesm8@gmail.com>
2020-03-02 10:27:58 -0700

    Finish user story 3 with passing models and feature tests

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital <ApplicationRecord
   validates_presence_of :name
   has_many :doctors
+
+  def total_doctor_count
+    self.doctors.count
+  end
+
+  def unique_universities
+    self.doctors.select(:university).distinct.pluck(:university)
+  end
 end
