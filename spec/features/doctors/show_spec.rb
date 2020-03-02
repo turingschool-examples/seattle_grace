8389c3282f634345d8288cc0beddc1881c46d239
jerry <jerry@jerrys-MacBook-Pro.local>
2020-03-02 09:32:41 -0700

    User story 1

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -3,22 +3,39 @@ require 'rails_helper'
 RSpec.describe 'As a User' do
   describe "Doc show page" do
     before(:each) do
-	@doc1 = Doctor.create(name: 'Meredith Grey', specialty: 'General Surgery',education: 'Harvard University')
-	@doc2 = Doctor.create(name: 'Alex Karev', specialty: 'Pediatric Surgery', education: 'Johns Hopkins University')
 	@hospital1 = Hospital.create( name: 'Grey Sloan Memorial Hospital')
 	@hospital2 = Hospital.create( name: 'Pacific Northwest General Hospital')
+	@doc1 = Doctor.create(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University', hospital: @hospital2)
+	@doc2 = Doctor.create(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University', hospital: @hospital1)
 	@patient1 = Patient.create( name: 'Katie Bryce', age: 24)
 	@patient2 = Patient.create( name: 'Denny Duquette', age: 39)
 	@patient3 = Patient.create( name: 'Rebecca Pope', age: 32)
+	@doc1.patients << @patient1
+	@doc1.patients << @patient2
 	visit "/doctors/#{@doc1.id}"
     end
 
 	it 'show doc info' do
-		within "doc" do
+		within "#doc" do
 			expect(page).to have_content("Meredith Grey")
 			expect(page).to have_content("General Surgery")
 			expect(page).to have_content("Harvard University")
 		end
 	end
 
+	it 'show hopital' do
+		within "#hospital" do
+			expect(page).to have_content("Pacific Northwest General Hospital")
+		end
+	end
 
+	it 'show patients' do
+		within "#patients" do
+			expect(page).to have_content("Katie Bryce")
+			expect(page).to have_content("Denny Duquette")
+		end
+		save_and_open_page
+	end
+
+   end
+end
