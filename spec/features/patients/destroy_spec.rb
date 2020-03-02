0e6f79f5ae203a19ecacf32e58031f3106b4caee
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-02 11:13:16 -0700

    Created test for user story 4

--- spec/features/patients/destroy_spec.rb
+++ spec/features/patients/destroy_spec.rb
@@ -13,11 +13,23 @@ RSpec.describe "Patients index page", type: :feature do
     patient_3 = doctor_2.patients.create(name: "Danny DeVito", age: "56")
 
 
-    visit "/hospitals/#{hospital_1.id}"
+    visit "/doctors/#{doctor_1.id}"
 
-    expect(page).to have_content(hospital_1.name)
-    expect(page).to have_content("Number of Doctors: 3")
-    expect(page).to have_content(doctor_1.university, count: 1)
-    expect(page).to have_content(doctor_2.university, count: 1)
+    within "#patient-#{patient_1.id}" do
+      expect(page).to have_content(patient_1.name)
+      expect(page).to have_button("Delete Patient")
+      click_on("Delete Patient")
+    end
+    expect(current_path).to eq("/doctors/#{doctor_1.id}")
+    expect(page).to_not have_content(patient_1.name)
   end
 end
+
+# User Story 4, Remove a Patient from a Doctor
+# ​
+# As a visitor
+# When I visit a Doctor's show page
+# Next to each patient's name, I see a button to remove that patient from that doctor's caseload
+# When I click that button for one patient
+# I'm brought back to the Doctor's show page
+# And I no longer see that patient's name listed
