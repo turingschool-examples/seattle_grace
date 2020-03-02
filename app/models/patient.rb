e49fd278508569a40b28cf5bf11c48cdf24c1978
William Meighan <williamdmeighan@gmail.com>
2020-03-02 09:51:26 -0700

    Add ordered_patients helper method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,9 @@ class Patient <ApplicationRecord
   validates_presence_of :name, :age
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
+
+
+  def self.ordered_patients
+    order(age: :desc)
+  end
 end
