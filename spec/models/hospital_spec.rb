69bca17f16a8a2c1ca0a42012d9f5350f37a6299
William Meighan <williamdmeighan@gmail.com>
2020-03-02 10:04:15 -0700

    Create model test for total_doctors helper method

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,10 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Hospital, type: :model do
+  before :each do
+    @hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
+
+    @doctor_1 = @hospital.doctors.create(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+    @doctor_2 = @hospital.doctors.create(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
+
+    @patient_1 = @doctor_1.patients.create(name: "Katie Bryce", age: 24)
+    @patient_2 = @doctor_1.patients.create(name: "Denny Duquette", age: 39)
+
+    @patients = Patient.all
+  end
+
   describe 'validations' do
     it {should validate_presence_of :name}
   end
+
   describe 'relationships' do
     it {should have_many :doctors}
   end
+
+  describe 'methods' do
+    it "total_doctors" do
+      expect(@hospital.total_doctors).to eq(2)
+    end
+  end
+
+
 end
