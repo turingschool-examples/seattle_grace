b9987612348d3aff5f3a5f152912faedcc9f8a38
Matt Holmes <holmesm8@gmail.com>
2020-03-02 09:17:55 -0700

    Add model test for many to many relationship of doctors and patients

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,13 +5,8 @@ RSpec.describe Patient, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
   end
-
+  describe 'relationships' do
+    it {should belong_to :doctor_patients}
+    it {should have_many(:doctors).through(:doctor_patients)}
+  end
 end
-
-# describe 'relationships' do
-#   it {should belong_to :doctor_patients}
-#   it {should have_many(:doctors).through(:doctor_patients)}
-# end
-
-# has_many :doctor_patients
-# has_many :doctors, through: :doctor_patients
