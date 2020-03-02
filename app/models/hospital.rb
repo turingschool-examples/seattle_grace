9b93b0576693a330e95777eecc6d955cdf8589bb
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-02 10:37:15 -0700

    add methods for hospital

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital <ApplicationRecord
   validates_presence_of :name
   has_many :doctors
+
+  def docs_university
+    doctors.select(:university).distinct.pluck(:university)
+  end
+
+  def number_of_docs
+    doctors.count
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Hospital, type: :model do
   describe 'relationships' do
     it {should have_many :doctors}
   end
+  describe 'methods' do
+    it 'should give unique list of doctor universities' do
+      hospital2 = Hospital.create!(name: 'Hospital in Boulder')
+      doctor2 = hospital2.doctors.create!(name: 'Steve', specialty: 'Adults', university: 'Yale')
+      doctor3 = hospital2.doctors.create!(name: 'Chris', specialty: 'Children', university: 'Yale')
+      doctor4 = hospital2.doctors.create!(name: 'Larry', specialty: 'Life', university: 'Stanford')
+
+      expect(hospital2.docs_university).to eq(['Stanford', 'Yale'])
+    end
+  end
 end
