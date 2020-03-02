fb81066f4e0723ec022806d70e9f37dc0ca6e1fc
sasloan <sebastiansloan94@gmail.com>
2020-03-02 09:09:06 -0700

    Deleted all unnecassary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	# hospitals
-	get '/hospitals', to: 'hospitals#index'
-	get '/hospitals/new', to: 'hospitals#new'
-	post '/hospitals', to: 'hospitals#create'
-	get '/hospitals/:id', to: 'hospitals#show'
-	get '/hospitals/:id/edit', to: 'hospitals#edit'
-	patch '/hospitals/:id', to: 'hospitals#update'
-	delete '/hospitals/:id', to: 'hospitals#destroy'
-
-	# doctors
-	get '/hospitals/:id/doctors', to: 'doctors#index'
-	get '/hospitals/:id/doctors/new', to: 'docotors#new'
-	post '/hospitals/:id/doctors', to: 'doctors#create'
-	get '/hospitals/:id/doctors/:id', to: 'doctors#show'
-	get '/hospitals/:id/doctors/:id/edit', to: 'doctors#edit'
-	patch '/hospitals/:id/doctors/:id', to: 'doctors#update'
-	delete '/hospitals/:id/doctors/:id', to: 'doctors#destroy'
-
+	
 end
