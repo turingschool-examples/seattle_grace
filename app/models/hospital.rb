5e1309b92a5234158e1e6e4166665b96ee5c4ab6
Elom Amouzou <elom1990@yahoo.com>
2020-03-02 10:36:08 -0700

    add hosptial instance methods; tests pass

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital <ApplicationRecord
   validates_presence_of :name
   has_many :doctors
+
+  def get_count_of_doctors
+    doctors.count
+  end
+
+  def get_universities
+    doctors.pluck(:university)
+  end
 end
