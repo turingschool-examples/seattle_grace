1cdc64a223ad76391eee5b9cc633f024eea7c78e
DeMarcus Kirby <tkd024@gmail.com>
2020-03-02 11:26:52 -0700

    Remove destroy from paitients_controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,13 +2,4 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.order(age: :desc)
   end
-
-  def destroy
-    doc_patients = DoctorPatient.where(patient_id: params[:id])
-    doc_patients.each do |doc_patient|
-      doc_patient.destroy
-    end
-    Patient.destroy(params[:id])
-    redirect_back(fallback_location: '/hospitals')
-  end
 end
