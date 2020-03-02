85821d66c851ea1c7b8fefa333115be58a72a705
Elom Amouzou <elom1990@yahoo.com>
2020-03-02 10:14:07 -0700

    add route for patients index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors, only: [:show]
+  resources :patients, only: [:index]
 
 end
