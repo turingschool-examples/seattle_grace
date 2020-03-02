5dcc74ca2239525b1dae65e34cf67d72c75caad6
jerry <jerry@jerrys-MacBook-Pro.local>
2020-03-02 10:05:53 -0700

    Add hospital show test

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -1,28 +1,24 @@
 require 'rails_helper'
   
 RSpec.describe 'As a User' do
-  describe "patient index page" do
+  describe "hospital show page" do
     before(:each) do
         @hospital1 = Hospital.create( name: 'Grey Sloan Memorial Hospital')
         @hospital2 = Hospital.create( name: 'Pacific Northwest General Hospital')
         @doc1 = Doctor.create(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University', hospital: @hospital2)
-        @doc2 = Doctor.create(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University', hospital: @hospital1)
+        @doc2 = Doctor.create(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University', hospital: @hospital2)
         @patient1 = Patient.create( name: 'Katie Bryce', age: 24)
         @patient2 = Patient.create( name: 'Denny Duquette', age: 39)
         @patient3 = Patient.create( name: 'Rebecca Pope', age: 32)
         @doc1.patients << @patient1
         @doc1.patients << @patient2
-        visit "/patients"
+        visit "/hospitals/#{@hospital2.id}"
     end
 
 
-	it "patients" do
-		expect(page).to have_content("Katie Bryce")
-		expect(page).to have_content("Denny Duquette")
-		expect(page).to have_content("Rebecca Pope")
+	it "hospital info" do
+		expect(page).to have_content("Pacific Northwest General Hospital")
 	end
-
-   end
+  end
 end
-
 
