03f6538cddf0240e37c42da66064ff64700913e3
Alex Gallant <agallant1201@gmail.com>
2020-03-02 11:15:41 -0700

    Remove unnecessary setup information

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -12,13 +12,9 @@ RSpec.describe Patient, type: :model do
 
   describe 'instance methods' do
     it ".order_by_age_from_oldest" do
-      grey_hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
-      meredith = grey_hospital.doctors.create(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
       alex = Patient.create(name: "Alex Gallant", age: 31)
       shane = Patient.create(name: "Shane Walton", age: 29)
       gordon = Patient.create(name: "Gordon Webb", age: 30)
-
-      meredith.patients << [alex, shane, gordon]
 
       expect(Patient.order_by_age_from_oldest.first).to eq(alex)
       expect(Patient.order_by_age_from_oldest.last).to eq(shane)
