382111249a9dc57cab08714263719b80db06921b
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-02 09:55:56 -0700

    Add patient index test and seeds for visual testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@hospital_1 = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+
+@doc_1 = @hospital_1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University')
+@doc_2 = @hospital_1.doctors.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University')
+
+@doc_1.patients.create!(name: 'Katie Bryce', age: 24)
+@doc_1.patients.create!(name: 'Denny Duquette', age: 39)
+@doc_2.patients.create!(name: 'Rebecca Pope', age: 32)
