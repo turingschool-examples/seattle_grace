28315512af8a19ecb1af32dfedb2bc56e8f2e785
Jenny Klich <jennyklich@gmail.com>
2020-03-02 10:29:31 -0700

    Add end of story 3

--- spec/features/hospital/show_spec.rb
+++ spec/features/hospital/show_spec.rb
@@ -26,6 +26,28 @@ RSpec.describe "Hospital Show Page" do
   end
 
   it "can see a unique list of universities" do
-    
+    hospital_1 = Hospital.create(name: "Seattle Grace Hospital")
+    hospital_2 = Hospital.create(name: "New York Hospital")
+    doctor_1 = Doctor.create(name: "Meredith Grey",
+                            specialty: "General Surgery",
+                            university: "Harvard University")
+    doctor_2 = Doctor.create(name: "Alex Karev",
+                            specialty: "Pediatric Surgery",
+                            university: "Johns Hopkins University")
+    doctor_3 = Doctor.create(name: "Derek McDreamy Shepherd",
+                            specialty: "Attending Surgeon",
+                            university: "Harvard University")
+    doctor_4 = Doctor.create(name: "iranda Bailey",
+                            specialty: "General Surgery",
+                            university: "Stanford University")
+    hospital_1.doctors << doctor_1
+    hospital_1.doctors << doctor_2
+    hospital_1.doctors << doctor_3
+    hospital_2.doctors << doctor_4
+
+    visit hospital_path(hospital_1)
+
+    expect(page).to have_content("Harvard University Johns Hopkins University")
+    expect(page).to_not have_content(doctor_4.name)
   end
 end
