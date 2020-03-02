c617ee9903ecc62564ae4786a8d36af89b7726bd
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-02 09:20:48 -0700

    Add patients DoctorPatients tables and doc show test start

---
+++ spec/features/doctors/doctor_show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'As a Visitor' do
+	describe "When I visit an doctor's show page" do
+		it 'I see name, specialty, university, hospital and list of its patients' do
+      hosp1 = Hospital.create!(name: "Mass General")
+      doc1 = hosp1.doctors.create!(name: "Hawkeye", specialty: "Thorasic", university: "UMass")
+      patient1 = doc1.patients.create!(name: "Katie Bryce",
+      age: 24)
+      patient2 = doc1.patients.create!(name: "Denny Duquette",
+      age: 39)
+      
+    end
+  end
+end(
+# As a visitor
+# When I visit a doctor's show page
+# I see all of that doctor's information including:
+#   - name
+#   - specialty
+#   - university where they got their doctorate
+# And I see the name of the hospital where this doctor works
+# And I see the names of all of the patients this doctor has
