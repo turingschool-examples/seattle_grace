d76aca3e003aa8d9aec8d1233335a00460285226
Matt Holmes <holmesm8@gmail.com>
2020-03-02 09:57:47 -0700

    Add ordering method in patient model, passes test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "doctors/:id", to: 'doctors#show'
-
+  get "/doctors/:id", to: 'doctors#show'
+  get "/patients", to: 'patients#index'
 end
