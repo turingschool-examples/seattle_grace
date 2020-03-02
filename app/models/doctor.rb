3f67cc42513e5dfe65138d5d8546eb67246e0aaa
Cassie Achzenick <caachzenick@gmail.com>
2020-03-02 09:17:10 -0700

    Add model tests for many to many relationships

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor <ApplicationRecord
   validates_presence_of :name, :specialty, :university
   belongs_to :hospital
+  has_many :doctor_patients
+  has_many :patients, through: :doctor_patients
 end
