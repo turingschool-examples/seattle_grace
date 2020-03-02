91c5e96be4f53ea04266c606d08021414e5cc302
David Atkins <atki1080@gmail.com>
2020-03-02 09:07:19 -0700

    Set up many-to-many relationships

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,5 @@
 class Patient <ApplicationRecord
   validates_presence_of :name, :age
-  has_many :doctors
+  has_many :doctor_patients
+  has_many :patients, through: :doctor_patients
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Patient, type: :model do
     it {should validate_presence_of :age}
   end
   describe 'relationships' do
-    it {should have_many :doctors}
+    it {should have_many :doctor_patients}
+    it {should have_many(:doctors).through(:doctor_patients)}
   end
 end
