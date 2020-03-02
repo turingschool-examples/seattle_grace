b245e54da2c1cf925c78525bff0eed83d4c7f0c8
Paul Debevec <paulmdebevec@gmail.com>
2020-03-02 09:52:32 -0700

    Add functionality for patient index page

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,8 @@ class Patient < ApplicationRecord
   validates_presence_of :name, :age
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
+
+  def self.oldest_to_youngest
+    Patient.order(age: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors, only: [:show]  
+  resources :doctors, only: [:show]
+  resources :patients, only: [:index]  
 end
