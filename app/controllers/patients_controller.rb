c14c896eea2a480d348042165758a119cb7d6640
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-02 11:12:24 -0700

    added destroy method to patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,6 +4,13 @@ class PatientsController < ApplicationController
     @patients = order_patients
   end
 
+  def destroy
+    doctor = Doctor.find(params[:doctor_id])
+    patient = doctor.patients.find(params[:patient_id])
+    DoctorPatient.find_by(patient_id: patient.id, doctor_id: doctor.id).delete
+    redirect_to "/doctors/#{doctor.id}"
+  end
+
   private
 
   def order_patients
