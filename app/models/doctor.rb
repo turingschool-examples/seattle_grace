945ddef3e607437e6d486349f957bad410ae7042
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-02 11:50:31 -0700

    Added comments describing extra function

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,3 +3,7 @@ class Doctor < ApplicationRecord
   has_many :patients
   belongs_to :hospital
 end
+
+# Because I completely forgot to set this up as a many-to-many and set it up as a one-to-meny, my has_many should look more like
+#     has_many :patients, through: :doctor_patients
+#     has_many :doctor_patients
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,3 +3,7 @@ class Patient < ApplicationRecord
 
   belongs_to :doctor
 end
+
+# Because I completely forgot to set this up as a many-to-many and set it up as a one-to-meny, my has_many should look more like
+#     has_many :doctors, through: :doctor_patients
+#     has_many :doctor_patients
