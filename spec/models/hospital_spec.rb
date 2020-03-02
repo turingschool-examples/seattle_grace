c1e653622b3d6cdb9026ddea5f13a518ed95aa03
Matt Holmes <holmesm8@gmail.com>
2020-03-02 10:19:03 -0700

    Add testing for unique universities

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -18,5 +18,16 @@ RSpec.describe Hospital, type: :model do
 
       expect(hs1.total_doctor_count).to eq(expected_result)
     end
+
+    it "can list unique universities" do
+      hs1 = Hospital.create!(name: "Johns Hopkins")
+      dr1 = hs1.doctors.create!(name: "Matt", specialty: "brains", university: "GVSU", )
+      dr2 = hs1.doctors.create!(name: "Meghan", specialty: "holistics", university: "Community College")
+      dr3 = hs1.doctors.create!(name: "Zeke", specialty: "kidney stuff", university: "Community College")
+
+      expected_result = [dr1.university, dr2.university]
+
+      expect(hs1.unique_universities).to eq(expected_result)
+    end
   end
 end
