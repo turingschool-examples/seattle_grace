58f9bc4cd5265a24022aecdaae23d4cbc4801111
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-02 09:39:45 -0700

    Add doctor patient relations add patient model test

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,7 @@
 class Doctor <ApplicationRecord
   validates_presence_of :name, :specialty, :university
   belongs_to :hospital
+
+  has_many :doctor_patients
+  has_many :patients, through: :doctor_patients
 end
