c880b3c55f6a79774013b50d8b389b687bfd4a3e
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-02 10:35:41 -0700

    Add user story 4

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,13 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.all
   end
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    @doctor = @patient.doctor
+
+    @patient.destroy
+
+    redirect_to doctor_path(@doctor)
+  end
 end
--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'As a VISITOR' do
   before :each do
     @hospital_1 = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
 
-    @doc_1 = @hospital_1.doctors.create!( name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University' )
-    @doc_2 = @hospital_1.doctors.create!( name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University' )
+    @doc_1 = @hospital_1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University')
+    @doc_2 = @hospital_1.doctors.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University')
 
-    @doc_1.patients.create!(name: 'Katie Bryce', age: 24 )
-    @doc_1.patients.create!( name: 'Denny Duquette', age: 39 )
-    @doc_2.patients.create!( name: 'Rebecca Pope', age: 32 )
+    @patient_1 = @doc_1.patients.create!(name: 'Katie Bryce', age: 24)
+    @patient_2 = @doc_1.patients.create!(name: 'Denny Duquette', age: 39)
+    @patient_3 = @doc_2.patients.create!(name: 'Rebecca Pope', age: 32)
   end
 
   describe 'when I visit a SHOW PAGE' do
@@ -32,6 +32,17 @@ RSpec.describe 'As a VISITOR' do
         expect(page).to have_content('Denny Duquette, 39')
         expect(page).to_not have_content('Rebecca Pope, age: 32')
       end
+    end
+
+    it 'can destroy a patient from list' do
+      visit "/doctors/#{@doc_1.id}"
+
+      within "#patient-#{@patient_1.id}" do
+        click_on 'Delete Patient'
+      end
+
+      expect(current_path).to eq("/doctors/#{@doc_1.id}")
+      expect(page).to_not have_content('Katie Bryce, 24')
     end
   end
 end
