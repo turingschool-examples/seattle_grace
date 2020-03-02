e38224d5d02fa2818f175c46012c314e42ec1988
Nathan Keller <keller.nathan@gmail.com>
2020-03-02 09:29:40 -0700

    Working through US1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,57 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 hospital_1 = Hospital.create(
-  name: "Denver Hospital"
+  name: "Grey Sloan Memorial Hospital"
+  )
+
+hospital_2 = Hospital.create(
+  name: "Pacific Northwest General Hospital"
   )
 
 doctor_1 = Doctor.create!(
-    name: "Dr. Nick",
-    specialty: "Hips",
+    name: "Meredith Grey",
+    specialty: "General Surgery",
     university: "Harvard",
     hospital: hospital_1
   )
 
 doctor_2 = Doctor.create!(
-    name: "Dr. Sally",
-    specialty: "Backs",
-    university: "Stanford",
+    name: "Alex Karev",
+    specialty: "Pediatric Surgery",
+    university: "Johns Hopkins University",
     hospital: hospital_1
+  )
+
+doctor_3 = Doctor.create!(
+    name: "Miranda Bailey",
+    specialty: "General Surgery",
+    university: "Stanford University",
+    hospital: hospital_2
+  )
+
+doctor_4 = Doctor.create!(
+    name: "Derek Webber",
+    specialty: "Attending Surgeon",
+    university: "University of Pennsylvania",
+    hospital: hospital_2
+  )
+
+patient_1 = Patient.create!(
+    name: "Katie Bryce",
+    age: 24,
+  )
+
+patient_2 = Patient.create!(
+    name: "Denny",
+    age: 39,
+  )
+
+patient_3 = Patient.create!(
+    name: "Rebecca Pope",
+    age: 24,
+  )
+
+patient_4 = Patient.create!(
+    name: "Zola Sheperd",
+    age: 2,
   )
---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+
+RSpec.describe "Doctor Show Page" type: :feature do
+    it "can see the doctors information" do
+
+      
+
+    end
+end
