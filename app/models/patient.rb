8df09f5a3e50e6f8171a46ba52b6320e047dd73a
Elom Amouzou <elom1990@yahoo.com>
2020-03-02 10:13:58 -0700

    add sorted class method for patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,8 @@ class Patient < ApplicationRecord
   validates_presence_of :name, :age
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
+
+  def self.sorted
+    self.order(age: :desc)
+  end
 end
