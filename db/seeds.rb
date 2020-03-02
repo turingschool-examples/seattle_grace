1f568813daf3b34e0bb71475014a0ae8da665147
sasloan <sebastiansloan94@gmail.com>
2020-03-02 09:08:30 -0700

    Add files for seed and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Patient.destroy_all 
+Doctor.destroy_all
+Hospital.destroy_all
+
+# hospitals
+
+@north = Hospital.create!(name: "North Hospital")
+@south = Hospital.create!(name: "South Hospital")
+@west = Hospital.create!(name: "west Hospital")
+
+# doctors
+
+@phil = @north.doctors.create!(name: 'Phil', specialty: "Love", university: "Love School")
+@john = @north.doctors.create!(name: 'John', specialty: "Pediactrics", university: "Hampden University")
+@hellen = @north.doctors.create!(name: 'Hellen', specialty: "cranial", university: "Colorado State University")
+
+@daniel = @south.doctors.create!(name: "Daniel", specialty: "Cyropractor", university: "Colorado University")
+@sherry = @south.doctors.create!(name: "Sherry", specialty: "Vetrinarian", university: "Red Rocks University")
+@holly = @south.doctors.create!(name: "Holly", specialty: "Birth", university: "Harvard University")
+
+@matt = @west.doctors.create!(name: "Matt", specialty: "Hand Work", university: "Brown University")
+@shawn = @west.doctors.create!(name: "Shawn", specialty: "plastic surgery", university: "California University")
+@dillon = @west.doctors.create!(name: "Dillon", specialty: "Necolepcy", university: "Main University")
+
+# patients
+
+@danny = Patient.create!(name: "Danny Moore", age: 35)
+@katie = Patient.create!(name: "Katie Bryce", age: 24)
+@denny = Patient.create!(name: "Denny Duquette", age: 39)
+@rebecca = Patient.create!(name: "Rebecca Pope", age: 32)
+@zola = Patient.create!(name: "Zola  Shepherd", age: 2)
