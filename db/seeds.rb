c7785a92c0f70d6f9007b32f2e74557c504da92d
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-02 11:21:24 -0700

    more seeds for further rails s testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Hospital.destroy_all
-Doctor.destroy_all
+DoctorPatient.destroy_all
 Patient.destroy_all
-
+Doctor.destroy_all
+Hospital.destroy_all
 hospital1 = Hospital.create!(name: 'Hospital in Denver')
 hospital2 = Hospital.create!(name: 'Hospital in Boulder')
 doctor1 = hospital1.doctors.create!(name: 'John', specialty: 'Surgeon', university: 'Harvard')
 doctor2 = hospital2.doctors.create!(name: 'Steve', specialty: 'Adults', university: 'Yale')
 doctor3 = hospital2.doctors.create!(name: 'Chris', specialty: 'Children', university: 'Yale')
 doctor4 = hospital2.doctors.create!(name: 'Larry', specialty: 'Life', university: 'Stanford')
+doctor5 = hospital2.doctors.create!(name: 'Susie', specialty: 'All Duties', university: 'Yale')
 patient1 = doctor1.patients.create!(name: 'Christian', age: 28)
+patient4 = doctor1.patients.create!(name: 'Mike', age: 31)
+patient5 = doctor1.patients.create!(name: 'Hannah', age: 37)
 patient2 = doctor2.patients.create!(name: 'Tanner', age: 26)
 patient3 = doctor2.patients.create!(name: 'Megan', age: 22)
 patient2 = doctor2.patients.create!(name: 'Sally', age: 42)
