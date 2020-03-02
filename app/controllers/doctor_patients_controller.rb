5913a1b2c0e66ed886f247cf59f674494bb5e7d3
Nathan Keller <keller.nathan@gmail.com>
2020-03-02 11:50:32 -0700

    Removing patient from caseload when a patient has multiple doctors mostly working

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -3,8 +3,8 @@ class DoctorPatientsController < ApplicationController
   def destroy
     doctor = Patient.find(params[:id]).doctors.first
     patient = Patient.find(params[:id])
+  
     doctor.patients.delete(patient)
-
     redirect_to "/doctors/#{doctor.id}"
   end
 end
--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -84,4 +84,53 @@ RSpec.describe "Doctor Show Page" do
 
     expect(page).to have_content(patient_2.name)
   end
+
+  it "shows a button to remove a when patient has multiple doctors" do
+    hospital_1 = Hospital.create(
+      name: "Grey Sloan Memorial Hospital"
+      )
+
+    doctor_1 = Doctor.create!(
+        name: "Meredith Grey",
+        specialty: "General Surgery",
+        university: "Harvard",
+        hospital: hospital_1
+      )
+
+    doctor_2 = Doctor.create!(
+        name: "Other Doctor",
+        specialty: "General Surgery",
+        university: "Place",
+        hospital: hospital_1
+      )
+
+    patient_1 = Patient.create!(
+        name: "Katie Bryce",
+        age: 24,
+      )
+
+    patient_2 = Patient.create!(
+        name: "Denny Smith",
+        age: 39,
+      )
+
+    doctor_1.patients << [patient_1, patient_2]
+    doctor_2.patients << [patient_1, patient_2]
+
+    visit "/doctors/#{doctor_1.id}"
+
+    within "#patient-#{patient_2.id}" do
+      click_on ("Remove Patient")
+    end
+
+    expect(current_path).to eq("/doctors/#{doctor_1.id}")
+    expect(page).not_to have_content(patient_2.name)
+    expect(page).not_to have_content(patient_2.age)
+
+    expect(page).to have_content(patient_1.name)
+
+    visit "/patients"
+
+    expect(page).to have_content(patient_2.name)
+  end
 end
