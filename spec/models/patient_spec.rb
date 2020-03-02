419dd7d36aa80b7909a3e1cd9f362a816212902e
Elom Amouzou <elom1990@yahoo.com>
2020-03-02 10:13:47 -0700

    add patient class method 'sorted' test

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe Patient, type: :model do
     it {should have_many :doctor_patients}
     it {should have_many(:doctors).through(:doctor_patients)}
   end
+
+  describe "class methods" do
+    before(:each) do
+      @hospital1 = Hospital.create(name: "Seattle Grace")
+      @hospital2 = Hospital.create(name: "New York Hospital")
+
+      @doctor1 = @hospital1.doctors.create(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+      @doctor2 = @hospital1.doctors.create(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
+
+      @patient1 = @doctor1.patients.create(name: "Katie Bryce", age: 24)
+      @patient2 = @doctor1.patients.create(name: "Denny Duquette", age: 39)
+      @patient3 = @doctor2.patients.create(name: "Rebecca Pope", age: 32)
+      @patient4 = @doctor2.patients.create(name: "Zola Shepherd", age: 2)
+
+      @patients = Patient.all
+    end
+
+    it "#sorted"  do
+      expect(@patients.sorted).to eq([@patient2, @patient3, @patient1, @patient4])
+    end
+  end
 end
