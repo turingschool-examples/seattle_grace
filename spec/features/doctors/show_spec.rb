8f68952330a36f2fb8538bb74b77b010add65299
Cassie Achzenick <caachzenick@gmail.com>
2020-03-02 10:13:39 -0700

    Add user story 4 feature test

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -31,4 +31,40 @@ RSpec.describe 'Doctors show page' do
     expect(page).to_not have_content("Pacific Northwest General Hospital")
     expect(page).to_not have_content("Denny Duquette")
   end
+
+  it 'A patient can be removed from a doctors workload' do
+    hospital1 = Hospital.create!(name: "Sacred Heart")
+    hospital2 = Hospital.create!(name: "Pacific Northwest General Hospital")
+
+    doctor1 = Doctor.create!(name: "Turk", specialty: "Surgery", university: "Colorado University", hospital: hospital1)
+    doctor2 = Doctor.create!(name: "Carla", specialty: "Nursing", university: "New Jersey University", hospital: hospital2)
+
+    patient1 = Patient.create!(name: 'Zola Shepherd', age: 2)
+    patient2 = Patient.create!(name: 'Rebecca Pope', age: 32)
+    patient3 = Patient.create!(name: 'Denny Duquette', age: 39)
+
+    doctor1.patients << patient1
+    doctor1.patients << patient2
+    doctor2.patients << patient3
+
+    visit "/doctors/#{doctor1.id}"
+
+    within "#patient-#{patient1.id}" do
+      expect(page).to have_button("Remove Patient")
+      expect(page).to have_content('Zola Shepherd')
+    end
+
+    expect(page).to_not have_content("Denny Duquette")
+
+    within "#patient-#{patient2.id}" do
+      expect(page).to have_content('Rebecca Pope')
+      click_button ("Remove Patient")
+    end
+
+    expect(current_path).to eq("/doctors/#{doctor1.id}")
+
+    expect(page).to_not have_content("Rebecca Pope")
+    expect(page).to_not have_content("Denny Duquette")
+    expect(page).to have_content("Zola Shepherd")
+  end
 end
