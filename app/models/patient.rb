7365fc6a25b8235d790d8299692ea2645fea89ed
David Atkins <atki1080@gmail.com>
2020-03-02 09:09:50 -0700

    Fix typo

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient <ApplicationRecord
   validates_presence_of :name, :age
   has_many :doctor_patients
-  has_many :patients, through: :doctor_patients
+  has_many :doctors, through: :doctor_patients
 end
