1bbfe731a471f8001e970f70bc71d9583d679f72
Kathleen Carroll <khakicarroll@comcast.net>
2020-03-02 11:16:39 -0700

    Update remove patient spec

--- spec/features/doctors/remove_patient_spec.rb
+++ spec/features/doctors/remove_patient_spec.rb
@@ -5,22 +5,15 @@ RSpec.describe 'When I visit a doctors show page' do
     seattle_grace = Hospital.create(name: 'Seattle Grace Hospital')
     katie = Patient.create(name: 'Katie Bryce', age: 24)
     denny = Patient.create(name: 'Denny Duquette', age: 39)
+    rebecca = Patient.create(name: 'Rebecca Pope', age: 32)
 
     meredith = Doctor.create(name: 'Meredith Grey',
                              specialty: 'General Surgery',
                              university: 'Harvard University',
                              hospital: seattle_grace,
-                             patients: [denny, katie])
+                             patients: [denny, katie, rebecca])
 
     visit "/doctors/#{meredith.id}"
-
-    expect(page).to have_content("#{meredith.name}")
-    expect(page).to have_content("Specialty: #{meredith.specialty}")
-    expect(page).to have_content("Education: #{meredith.university}")
-    expect(page).to have_content("Hospital: #{meredith.hospital.name}")
-    expect(page).to have_content("Patients:")
-    expect(page).to have_content("#{denny.name}")
-    expect(page).to have_content("#{katie.name}")
 
     within("#patient-#{denny.id}") do
       expect(page).to have_content("#{denny.name}")
@@ -29,6 +22,15 @@ RSpec.describe 'When I visit a doctors show page' do
     end
 
     expect(page).to_not have_content("#{denny.name}")
+    expect(page).to have_content("#{katie.name}")
+    expect(page).to have_content("#{rebecca.name}")
+
+    within("#patient-#{rebecca.id}") do
+      click_on("Remove Patient")
+      expect(current_path).to eq("/doctors/#{meredith.id}")
+    end
+
+    expect(page).to_not have_content("#{rebecca.name}")
     expect(page).to have_content("#{katie.name}")
   end
 end
