393c0ead317cb8aa657b4bfcecff0695cd3cf388
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-03-02 10:55:34 -0700

    add delete route for doctors patients

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/doctors/:id', to: 'doctors#show'
   get '/hospitals/:id', to: 'hospitals#show'
   get '/patients', to: 'patients#index'
+  delete '/doctors/:id/patients/:patient_id', to: 'doctor_patients#destroy'
 end
