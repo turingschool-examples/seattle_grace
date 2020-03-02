78808443ba05b9f965f64788a3d4e58efedb45ee
DeMarcus Kirby <tkd024@gmail.com>
2020-03-02 09:32:00 -0700

    Add test for doctor show page

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor to doctor show page', type: :feature do
+  before :each do
+    @hospital = Hospital.create!(name: 'Parkland Hospital')
+    @tron = @hospital.doctors.create!(name: 'Dr. Tron Kattington', specialty: 'Attending Surgeon', university: 'University of Mind Ya Business')
+    @patient1 = @tron.patients.create!(name: 'Joe', age: 20)
+    @patient2 = @tron.patients.create!(name: 'Becky', age: 22)
+    @patient3 = Patient.create!(name: 'John', age: 20)
+  end
+  ​it 'should show a doctors information' do
+    visit "/doctors/{#{@tron.id}}"
+    expect(page).to have_content(@tron.name)
+    expect(page).to have_content(@tron.specialty)
+    expect(page).to have_content(@tron.university)
+    expect(page).to have_content(@hospital.name)
+    expect(page).to have_content(@patient1.name)
+    expect(page).to have_content(@patient2.name)
+    expect(page).to_not have_content(@patient3.name)
+  end
+
+end
