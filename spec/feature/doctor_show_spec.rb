96552035b117cb03bb998b795b769fc8b66ec4d8
Matt Holmes <holmesm8@gmail.com>
2020-03-02 09:43:07 -0700

    Finish user story 1

--- spec/feature/doctor_show_spec.rb
+++ spec/feature/doctor_show_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.describe 'as a visitor', type: :feature do
   describe 'when I visit the doctor show page' do
     it 'can see all of that doctors information including name, specialty, university, name of hos and all patients' do
-      dr1 = Doctor.create!(name: "Matt", specialty: "brains", university: "GVSU")
-      dr2 = Doctor.create!(name: "Meghan", specialty: "holistics", university: "Community College")
-      hs1 = dr1.hospitals.create!(name: "Johns Hopkins")
-      hs2 = dr2.hospitals.create!(name: "Hope")
-      pt1 = Patient.create!(name: "Kathleen", age: 10)
-      pt2 = Patient.create!(name: "Zeke", age: 20)
-      dp1 = DoctorPatients.create!(doctor_id: dr1.id, patient_id: pt1.id)
-      dp2 = DoctorPatients.create!(doctor_id: dr2.id, patient_id: pt2.id)
+      hs1 = Hospital.create!(name: "Johns Hopkins")
+      hs2 = Hospital.create!(name: "Hope")
+      dr1 = hs1.doctors.create!(name: "Matt", specialty: "brains", university: "GVSU", )
+      dr2 = hs2.doctors.create!(name: "Meghan", specialty: "holistics", university: "Community College")
+      pt1 = dr1.patients.create!(name: "Kathleen", age: 10)
+      pt2 = dr1.patients.create!(name: "Zeke", age: 20)
+      pt3 = dr2.patients.create!(name: "Meg", age: 20)
 
       visit "doctors/#{dr1.id}"
       expect(page).to have_content(dr1.name)
@@ -18,6 +17,8 @@ RSpec.describe 'as a visitor', type: :feature do
       expect(page).to have_content(dr1.university)
       expect(page).to have_content(hs1.name)
       expect(page).to have_content(pt1.name)
+      expect(page).to have_content(pt2.name)
+      expect(page).to_not have_content(pt3.name)
     end
   end
 end
