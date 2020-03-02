e689728241cd659fb190813fd79ccf23d4de3834
Elom Amouzou <elom1990@yahoo.com>
2020-03-02 11:06:35 -0700

    add route for patient delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors, only: [:show]
-
+  resources :doctors, only: [:show] do
+    resources :patients, only: [:destroy]
+  end
+  
   resources :patients, only: [:index]
 
   resources :hospitals, only: [:show]
