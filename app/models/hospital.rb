bed9967dbacd38c6f8512e6138872a980937cc6d
Cassie Achzenick <caachzenick@gmail.com>
2020-03-02 10:05:05 -0700

    Add finished user story 3

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital <ApplicationRecord
   validates_presence_of :name
   has_many :doctors
+
+  def total_doctors
+    doctors.count
+  end
+
+  def unique_universities
+    doctors.pluck(:university).uniq
+  end
 end
